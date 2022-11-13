1e46b9f27aa4d3a57f87b1e1f23eb90dedf1321b
Gleb Kaygorodtsev <kaygorodcevg@mail.ru>
2022-11-13 15:12:31 +0500

    hotfix: исправления после 1 ревью

--- script/index.js
+++ script/index.js
@@ -1,44 +1,32 @@
-const editPopup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-icon");
-const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-icon');
+const editButton = document.querySelector('.profile__edit-button');
+const formElement = document.querySelector('.popup__form'); // Находим форму в DOM
+const nameInput = formElement.querySelector('.popup__input_name'); // Находим поля формы в DOM
+const jobInput = formElement.querySelector('.popup__input_job'); // Находим поля формы в DOM
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 // закрытие popup //
 function popupClose() {
-  editPopup.classList.remove("popup__opened");
+  editPopup.classList.remove('popup_opened');
 }
-closeButton.addEventListener("click", popupClose);
 
 // открытие popup //
 function openPopup() {
-  editPopup.classList.add("popup__opened");
+  editPopup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
-editButton.addEventListener("click", openPopup);
 
 // Изменения в форму //
-const formElement = document.querySelector(".popup__form"); // Находим форму в DOM
-const nameInput = formElement.querySelector(".popup__input-name"); // Находим поля формы в DOM
-const jobInput = formElement.querySelector(".popup__input-job"); // Находим поля формы в DOM
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
-  document.querySelector(".profile__title").textContent = nameInput.value;
-  document.querySelector(".profile__subtitle").textContent = jobInput.value;
+ profileTitle.textContent = nameInput.value;
+ profileSubtitle.textContent = jobInput.value;
   popupClose();
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
-
-// reaction //
-const elements = document.querySelector(".elements");
-const reaction = elements.querySelectorAll(".elements__reaction");
-
-function addReaction() {
-  for (let i = 0; i < reaction.length; i++) {
-    reaction[i].addEventListener("click", function () {
-      reaction[i].classList.toggle("elements__reaction-active");
-    });
-  }
-}
-
-addReaction();
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
